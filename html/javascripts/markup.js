2417af30b98e70acabb65a8d298879eaf9d0d340
Ruslan Kozlov <ruslan@pieoneers.com>
2012-06-23 00:48:50 +0800

    pages

--- html/javascripts/markup.js
+++ html/javascripts/markup.js
@@ -1,8 +1,8 @@
 $(function(){
-        $("#learn_more_switcher").click(function () {
+        $(".learn_more_link").click(function () {
           $("div.learn_more").slideToggle("slow", function () {
-            $('div.learn_more').toggleClass('visible');
-            if($('div.learn_more').hasClass('visible')) {
+            $("div.learn_more").toggleClass("visible");
+            if($("div.learn_more").hasClass("visible")) {
               var position = $("div.learn_more").offset();
               $(window).scrollTop(position.top-20);
               $(".arrow").css("background-position", "50% -24px");
