fbc2c7a4923bf32e0778643833527488a4426291
unknown <Das Einhorn@.(none)>
2012-06-22 11:45:10 +0800

    pages

--- html/javascripts/markup.js
+++ html/javascripts/markup.js
@@ -1,13 +1,13 @@
 $(function(){
-        $(".learn_more_switcher").click(function () {
+        $("#learn_more_switcher").click(function () {
           $("div.learn_more").slideToggle("slow", function () {
             $('div.learn_more').toggleClass('visible');
             if($('div.learn_more').hasClass('visible')) {
               var position = $("div.learn_more").offset();
               $(window).scrollTop(position.top-20);
-              $("#learn_more_switcher").css("background-position", "200px -21px");
+              $(".arrow").css("background-position", "50% -24px");
             } else {
-              $("#learn_more_switcher").css("background-position", "200px 3px");
+              $(".arrow").css("background-position", "50% 0");
             }
             }
           );
