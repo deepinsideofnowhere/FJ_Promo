a88c4f70826696b101234d99b89847e1fcddb311
amanshin <andrew.manshin@me.com>
2012-06-22 18:07:02 -0700

    index cleanup

--- html/javascripts/markup.js
+++ html/javascripts/markup.js
@@ -1,15 +1,13 @@
 $(function(){
-        $(".learn_more_link").click(function () {
-          $("div.learn_more").slideToggle("slow", function () {
-            $("div.learn_more").toggleClass("visible");
-            if($("div.learn_more").hasClass("visible")) {
-              var position = $("div.learn_more").offset();
-              $(window).scrollTop(position.top-20);
-              $(".arrow").css("background-position", "50% -24px");
-            } else {
-              $(".arrow").css("background-position", "50% 0");
-            }
-            }
-          );
-        });
-      });
+  $(".learn_more_link").click(function () {
+    $("div.learn_more").slideToggle("slow", function () {
+      $("div.learn_more").toggleClass("visible");
+      if($("div.learn_more").hasClass("visible")) {
+        $(".arrow").css("background-position", "50% -24px");
+      } else {
+        $(".arrow").css("background-position", "50% 0");
+      }
+      }
+    );
+  });
+});
